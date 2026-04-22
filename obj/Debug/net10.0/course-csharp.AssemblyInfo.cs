5a0920f3741a1322d5798561317de608987c9c51
UrielCisneros <urielcisne96@gmail.com>
2026-04-22 13:28:04 -0600

    Add: Se agregan condiguracion para static console system

--- obj/Debug/net10.0/course-csharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/course-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("course-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f639c388c2eb73ed0dc72b1ea321e69eaec4c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27295fd7ebca305ca872b9068744c282ab918de")]
 [assembly: System.Reflection.AssemblyProductAttribute("course-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("course-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
